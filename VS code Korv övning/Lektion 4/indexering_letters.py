c3cd32ac8d1038653364d45b36bf7e7932c799cc
Kevin Wanisian <kevin.wanisian00@gmail.com>
2023-09-04 10:46:56 +0200

    Indexering

---
+++ VS code Korv övning/Lektion 4/indexering_letters.py
@@ -0,0 +1,19 @@
+str1 = 1
+
+letters = 'ABC'
+print(letters[0])  # A
+print(letters[1])  # B
+print(letters[2])  # C
+
+print("Första tecknet", str1)
+
+letters = "ABC"
+
+i = 0
+while i < len(letters):
+    print(letters[i])
+    i += 1
+
+for letter in letters:
+    print(letter)
+# samma kod fast med for-sats
---
+++ VS code Korv övning/Lektion 4/stings_demo.py
@@ -0,0 +1,15 @@
+str1 = "Hej"
+str2 = "världen"
+result = str1 + " " + str2
+print(result)
+print("längen på strängen är :", len(result))
+
+print("Lite text och annat smått och gott. \nlite mer text och annat")
+
+print("""
+    Rad1:\tett
+    Rad2:\tTvå
+    Rad3:\tTre
+""")
+
+print("exempel på escapa tecken är:\\n som betyder ny rad")
